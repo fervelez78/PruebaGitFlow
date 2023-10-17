8b2a7cb5648cf73d281859f20be1c28c0381f2e6
fervelez78 <146050250+fervelez78@users.noreply.github.com>
2023-10-16 20:16:56 -0600

    Carga inicial del arquetipo por proyecto padre

---
+++ Materias/src/main/java/com/minsait/curso/MateriasApplication.java
@@ -0,0 +1,13 @@
+package com.minsait.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateriasApplication.class, args);
+	}
+
+}
